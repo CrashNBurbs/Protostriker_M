ad77c41d2c9b40b4dd759ad557e7008fd12ea8e9
Will Taplin <wtaplin@yahoo.com>
2012-12-15 03:37:10 -0500

    Is this thing on?

--- weapons.py
+++ weapons.py
@@ -22,19 +22,19 @@ class BasicWeapon():
             shot = None
         return shot
 
-class Spreader():
-    def __init__(self, game):
-        self.sound = game.sound_manager.get_sound('laser')
-        self.bullet_image = game.image_manager.get_image('pshot')
-        self.speed = 200
-        self.last_shot = 0
+#class Spreader():
+#    def __init__(self, game):
+#        self.sound = game.sound_manager.get_sound('laser')
+#        self.bullet_image = game.image_manager.get_image('pshot')
+#        self.speed = 200
+#        self.last_shot = 0
 
-    def fire(self, current_time, player_rect):
-        if current_time - self.last_shot > self.speed:
-            shot = bullets.BasicBullet(player_rect.right - 6,
-                             player_rect.centery + 4, self.bullet_image)
-            self.sound.play()
-            self.last_shot = current_time
-        else:
-            shot = None
-        return shot
+#    def fire(self, current_time, player_rect):
+#        if current_time - self.last_shot > self.speed:
+#            shot = bullets.BasicBullet(player_rect.right - 6,
+#                             player_rect.centery + 4, self.bullet_image)
+#            self.sound.play()
+#            self.last_shot = current_time
+#        else:
+#            shot = None
+#        return shot
