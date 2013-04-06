9a0720d0d323cbcafc9731fbf78e3cff73ee409b
Will Taplin <wtaplin@yahoo.com>
2013-04-06 14:10:31 -0400

    changed draw order so enemy bullets are never hiden

--- sprite_manager.py
+++ sprite_manager.py
@@ -40,8 +40,8 @@ class SpriteManager(engine.objects.SpriteManager):
         self.enemy_queue = [] # list of offscreen enemies
         self.update_order = ['player_group',  'enemy_group', 'player_shots',
                              'enemy_shots', 'powerups', 'explosions']
-        self.draw_order = ['player_shots', 'player_group', 'enemy_shots',
-                           'enemy_group', 'powerups', 'explosions']
+        self.draw_order = ['player_group', 'enemy_group', 'powerups', 
+                           'explosions','player_shots','enemy_shots']
 
     def update(self, current_time, viewport, player_rect):
         # update all sprites in the game
