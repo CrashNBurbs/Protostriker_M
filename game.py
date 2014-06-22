fe3d49ef8f8f76890424a4f305fe6cedb50a8d59
Will Taplin <wtaplin@yahoo.com>
2014-06-22 12:53:48 -0400

    sound effect volume tweak

--- game.py
+++ game.py
@@ -140,7 +140,7 @@ class PsmGame(engine.system.Game):
         self.sound_manager.load_sound('spreader.wav', 'spreader',
                                       volume = 0.2)
         self.sound_manager.load_sound('laserbeam.wav', 'laserbeam',
-                                      volume = 0.3)
+                                      volume = 0.1)
         self.sound_manager.load_sound('powerup.wav', 'powerup', volume = 0.5)
         self.sound_manager.load_sound('changeweapon.wav', 'changeweapon',
                                       volume = 0.5)
